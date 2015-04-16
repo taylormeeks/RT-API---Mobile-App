6c5df0b7fd7e1e1322441db906e0c9c51e7b070c
Omar Reyes <reyomar80@hotmail.com>
2015-04-15 18:07:49 -0700

    Test

--- Assignment4CSE394 2/Assignment4CSE394/MovieManager.h
+++ Assignment4CSE394 2/Assignment4CSE394/MovieManager.h
@@ -11,7 +11,7 @@
 
 #import <Foundation/Foundation.h>
 @interface MovieManager : NSObject
-
+//
 @property (nonatomic) NSMutableArray *movieList;
 @property (nonatomic) NSMutableArray *movieTypeList;
 @property (nonatomic) NSMutableArray *watchedList;
